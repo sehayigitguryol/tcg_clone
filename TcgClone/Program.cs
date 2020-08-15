83c0c8c38e065d7170bc846bf0f6a137d4e416eb
Seha Yiğit Güryol <s.yigitguryol@gmail.com>
2020-08-15 10:33:35 +0300

    initial project & gitignore update

---
+++ TcgClone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcgClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
